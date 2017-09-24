7ab0b6457242b1764b89b974a7039cd12f347afc
BriOD <briodonoghue@gmail.com>
2017-09-24 12:05:03 -0400

    create new user and check if user exists when signing in

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +13,18 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('AT:', accessToken);
-      console.log('RT:', refreshToken);
-      console.log('P:', profile);
+      User.findOne({ googleId: profile.id }) // check to see if the user already exists. query the db
+        .then(existingUser => {
+          //async action, so we need to handle it with a promise
+          done(null, existingUser);
+          if (existingUser) {
+            // we already have a record with giver profile id
+          } else {
+            new User({ googleId: profile.id })
+              .save() // grab the id off the profile and create a new user
+              .then(user => done(null, user)); // async action
+          }
+        });
     }
   )
 );
