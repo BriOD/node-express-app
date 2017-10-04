ca6d1783270947039f454714a43ecacba3706acd
BriOD <briodonoghue@gmail.com>
2017-10-04 17:12:24 -0400

    wrote function to create a new tourney from post req

--- routes/tourneyRoutes.js
+++ routes/tourneyRoutes.js
@@ -1,5 +1,18 @@
+const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 
+const Tourney = mongoose.model('tourneys');
+
 module.exports = app => {
-  app.post('/api/tourneys', requireLogin, (req, res) => {});
+  app.post('/api/tourneys', requireLogin, (req, res) => {
+    const { venue, buyin, date, receipt } = req.body;
+
+    const tourney = new Tourney({
+      venue,
+      buyin,
+      date,
+      receipt,
+      _user: req.user.id
+    });
+  });
 };
