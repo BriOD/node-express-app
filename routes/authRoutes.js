a46f0a172056963ddad02c01f960eae57aa6e180
BriOD <briodonoghue@gmail.com>
2017-10-23 16:36:00 -0400

    fixed where a user is redirected to when logging in

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,7 +14,7 @@ module.exports = app => {
     '/auth/google/callback',
     passport.authenticate('google'),
     (req, res) => {
-      res.redirect('/tournaments');
+      res.redirect('/tourneys');
     }
   );
 
