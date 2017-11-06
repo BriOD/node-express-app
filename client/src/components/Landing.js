67be398f72e88267cc09ea8b03e71aa0feee5d83
BriOD <briodonoghue@gmail.com>
2017-11-06 15:36:26 -0500

    added image to landing page

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import baxImage from '../images/bax-win.jpg';
 
 const Landing = () => {
   return (
     <div className="landing">
       <h1>Welcome to Team Bax!!!!</h1>
+      <img src="https://i.imgur.com/iC5XZkgm.jpg" />
     </div>
   );
 };
